bc62fbb97c918d77f3071dd13c42d86f0c17a596
一九五 <2259761058@qq.com>
2021-11-06 11:06:52 +0800

    Create

---
+++ example/tag_1306/tag_1306.ino
@@ -0,0 +1,317 @@
+/**
+ * 
+ * @todo
+ *  - move strings to flash (less RAM consumption)
+ *  - fix deprecated convertation form string to char* startAsTag
+ *  - give example description
+ */
+#include <SPI.h>
+#include "DW1000Ranging.h"
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// #define DEBUG
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+
+#define UWB_RST 27 // reset pin
+#define UWB_IRQ 34 // irq pin
+#define UWB_SS 4   // spi select pin
+
+#define I2C_SDA 32
+#define I2C_SCL 33
+
+typedef struct Link
+{
+    uint16_t anchor_addr;
+    float range;
+    float dbm;
+    struct Link *next;
+} link;
+
+Link *uwb_data;
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Wire.begin(I2C_SDA, I2C_SCL);
+    delay(1000);
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3C for 128x32
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+    display.clearDisplay();
+
+    logoshow();
+
+    //init the configuration
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(UWB_RST, UWB_SS, UWB_IRQ); //Reset, CS, IRQ pin
+    //define the sketch as anchor. It will be great to dynamically change the type of module
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachNewDevice(newDevice);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+    //Enable the filter to smooth the distance
+    //DW1000Ranging.useRangeFilter(true);
+
+    //we start the module as a tag
+    DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+
+    uwb_data = init_link();
+}
+
+long int runtime = 0;
+
+void loop()
+{
+    DW1000Ranging.loop();
+    if ((millis() - runtime) > 1000)
+    {
+        display_uwb(uwb_data);
+        runtime = millis();
+    }
+}
+
+void newRange()
+{
+    Serial.print("from: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+    Serial.print("\t Range: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+    Serial.print(" m");
+    Serial.print("\t RX power: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+    Serial.println(" dBm");
+
+    fresh_link(uwb_data, DW1000Ranging.getDistantDevice()->getShortAddress(), DW1000Ranging.getDistantDevice()->getRange(), DW1000Ranging.getDistantDevice()->getRXPower());
+    //print_link(uwb_data);
+}
+
+void newDevice(DW1000Device *device)
+{
+    Serial.print("ranging init; 1 device added ! -> ");
+    Serial.print(" short:");
+    Serial.println(device->getShortAddress(), HEX);
+
+    add_link(uwb_data, device->getShortAddress());
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+    Serial.print("delete inactive device: ");
+    Serial.println(device->getShortAddress(), HEX);
+
+    delete_link(uwb_data, device->getShortAddress());
+}
+
+//Data Link
+
+link *init_link()
+{
+#ifdef DEBUG
+    Serial.println("init_link");
+#endif
+    link *p = (link *)malloc(sizeof(link));
+    p->next = NULL;
+    p->anchor_addr = 0;
+    p->range = 0.0;
+
+    return p;
+}
+
+void add_link(link *p, uint16_t addr)
+{
+#ifdef DEBUG
+    Serial.println("add_link");
+#endif
+    link *temp = p;
+    //Find link end
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    Serial.println("add_link:find link end");
+    //Create a anchor
+    link *a = (link *)malloc(sizeof(link));
+    a->anchor_addr = addr;
+    a->range = 0.0;
+    a->dbm = 0.0;
+    a->next = NULL;
+
+    //Add anchor to end of link
+    temp->next = a;
+
+    return;
+}
+
+link *find_link(link *p, uint16_t addr)
+{
+#ifdef DEBUG
+    Serial.println("find_link");
+#endif
+    if (addr == 0)
+    {
+        Serial.println("find_link:Input addr is 0");
+        return NULL;
+    }
+
+    if (p->next == NULL)
+    {
+        Serial.println("find_link:Link is empty");
+        return NULL;
+    }
+
+    link *temp = p;
+    //Find target link or link end
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+        if (temp->anchor_addr == addr)
+        {
+            // Serial.println("find_link:Find addr");
+            return temp;
+        }
+    }
+
+    Serial.println("find_link:Can't find addr");
+    return NULL;
+}
+
+void fresh_link(link *p, uint16_t addr, float range, float dbm)
+{
+#ifdef DEBUG
+    Serial.println("fresh_link");
+#endif
+    link *temp = find_link(p, addr);
+    if (temp != NULL)
+    {
+
+        temp->range = range;
+        temp->dbm = dbm;
+        return;
+    }
+    else
+    {
+        Serial.println("fresh_link:Fresh fail");
+        return;
+    }
+}
+
+void print_link(link *p)
+{
+#ifdef DEBUG
+    Serial.println("print_link");
+#endif
+    link *temp = p;
+
+    while (temp->next != NULL)
+    {
+        //Serial.println("Dev %d:%d m", temp->next->anchor_addr, temp->next->range);
+        Serial.println(temp->next->anchor_addr, HEX);
+        Serial.println(temp->next->range);
+        Serial.println(temp->next->dbm);
+        temp = temp->next;
+    }
+
+    return;
+}
+
+void delete_link(link *p, uint16_t addr)
+{
+#ifdef DEBUG
+    Serial.println("delete_link");
+#endif
+    if (addr == 0)
+        return;
+
+    link *temp = p;
+    while (temp->next != NULL)
+    {
+        if (temp->next->anchor_addr == addr)
+        {
+            link *del = temp->next;
+            temp->next = del->next;
+            free(del);
+            return;
+        }
+        temp = temp->next;
+    }
+    return;
+}
+
+//SSD1306
+
+void logoshow(void)
+{
+    display.clearDisplay();
+
+    display.setTextSize(2);              // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);             // Start at top-left corner
+    display.println(F("Makerfabs"));
+
+    display.setTextSize(1);
+    display.setCursor(0, 20); // Start at top-left corner
+    display.println(F("DW1000 DEMO"));
+    display.display();
+    delay(2000);
+}
+
+void display_uwb(Link *p)
+{
+    link *temp = p;
+    int row = 0;
+
+    display.clearDisplay();
+    
+    display.setTextColor(SSD1306_WHITE);
+
+    if (temp->next == NULL)
+    {
+        display.setTextSize(2);
+        display.setCursor(0, 0);
+        display.println("No Anchor");
+        display.display();
+        return;
+    }
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+
+        //Serial.println("Dev %d:%d m", temp->next->anchor_addr, temp->next->range);
+        Serial.println(temp->anchor_addr, HEX);
+        Serial.println(temp->range);
+
+        char c[30];
+
+        //sprintf(c, "%X:%.1f m %.1f", temp->anchor_addr, temp->range, temp->dbm);
+        sprintf(c, "%X:%.1f m", temp->anchor_addr, temp->range);
+        display.setTextSize(2);
+        display.setCursor(0, row++ * 32); // Start at top-left corner
+        display.println(c);
+
+        sprintf(c, "%.2f dbm",temp->dbm);
+        display.setTextSize(1);
+        display.println(c);
+
+        if (row >= 2)
+        {
+            break;
+        }
+    }
+    delay(100);
+    display.display();
+    return;
+}
---
+++ readme.md
@@ -0,0 +1,135 @@
+# Makerfabs ESP32 UWB
+
+```c++
+/*
+Version:		V1.0
+Author:			Vincent
+Create Date:	2021/11/6
+Note:
+	
+*/
+```
+![](md_pic/main.jpg)
+
+
+[toc]
+
+# Makerfabs
+
+[Makerfabs home page](https://www.makerfabs.com/)
+
+[Makerfabs Wiki](https://makerfabs.com/wiki/index.php?title=Main_Page)
+
+# Makerfabs ESP32 UWB
+## Intruduce
+
+Product Link ：[]() 
+
+Wiki Link : []() 
+
+Makerfabs ESP32 UWB contains an ESP32 and a DW1000 chip.
+
+The DW1000 is a single-chip wireless transceiver based on ultra-wideband technology. The chip enables you to develop cost-effective RTLS solutions that can pinpoint indoor and outdoor positioning to within 10 cm.
+
+The chip provides a new approach to real-time location and indoor location systems, location-based services, wireless sensor networks and the Internet of Things by providing accurate location awareness and communication.
+
+
+
+
+## Feature
+
+- Integrated ESP32 2.4G WiFi and Bluetooth.
+- DW1000 UWB module.
+
+
+### Front:
+
+![front](md_pic/front.jpg)
+
+### Back:
+![back](md_pic/back.jpg)
+
+
+# Example
+
+## Equipment list
+
+
+- At least two ESP32 UWB
+
+
+## Compiler Options
+
+**If you have any questions，such as how to install the development board, how to download the code, how to install the library. Please refer to :[Makerfabs_FAQ](https://github.com/Makerfabs/Makerfabs_FAQ)**
+
+- Install board : ESP32 .
+- Install library : arduino-dw1000
+- Change code in DW1000.cpp
+
+```c++
+void DW1000Class::begin(uint8_t irq, uint8_t rst) {
+	// generous initial init/wake-up-idle delay
+	delay(5);
+	// Configure the IRQ pin as INPUT. Required for correct interrupt setting for ESP8266
+    	pinMode(irq, INPUT);
+	// start SPI
+	SPI.begin();
+
+// Change By VINCENT
+// #ifndef ESP8266
+// 	SPI.usingInterrupt(digitalPinToInterrupt(irq)); // not every board support this, e.g. ESP8266
+// #endif
+
+	// pin and basic member setup
+	_rst        = rst;
+	_irq        = irq;
+	_deviceMode = IDLE_MODE;
+	// attach interrupt
+	//attachInterrupt(_irq, DW1000Class::handleInterrupt, CHANGE); // todo interrupt for ESP8266
+	// TODO throw error if pin is not a interrupt pin
+	attachInterrupt(digitalPinToInterrupt(_irq), DW1000Class::handleInterrupt, RISING); // todo interrupt for ESP8266
+}
+```
+
+Comment out these code, or a compilation error will occur.
+
+```c++
+// #ifndef ESP8266
+// 	SPI.usingInterrupt(digitalPinToInterrupt(irq)); // not every board support this, e.g. ESP8266
+// #endif
+```
+
+- Upload code, select board "ESP32 DEV"
+
+
+## Example List
+
+### Anchor
+
+Distance test anchor point.
+
+### Tag
+
+Distance to the receiving end of the test. You need an anchor point to receive data.
+
+### Tag_1306
+
+There is an SSD1306 screen receiver, and a data structure for storing multiple anchor points is implemented. (The screen can display a maximum of two anchors, you can modify the font size to show more anchors). You need an anchor point to receive data.
+
+
+## Code Explain
+
+- Define Pins
+
+```c++
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+
+// connection pins
+const uint8_t PIN_RST = 27; // reset pin
+const uint8_t PIN_IRQ = 34; // irq pin
+const uint8_t PIN_SS = 4;   // spi select pin
+```
+

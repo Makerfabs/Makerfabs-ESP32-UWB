c7887192d8fe8246142e5d0964b0ca929362aeca
一九五 <2259761058@qq.com>
2023-01-15 13:40:04 +0800

    Create anchor_display.ino

---
+++ example/OutdoorPositioning_display/anchor_display/anchor_display.ino
@@ -0,0 +1,101 @@
+/*
+
+For ESP32 UWB Pro with Display
+
+*/
+
+#include <SPI.h>
+#include "DW1000Ranging.h"
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define ANCHOR_ADD "86:17:5B:D5:A9:9A:E2:9C"
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+
+#define UWB_RST 27 // reset pin
+#define UWB_IRQ 34 // irq pin
+#define UWB_SS 21  // spi select pin
+
+#define I2C_SDA 4
+#define I2C_SCL 5
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Wire.begin(I2C_SDA, I2C_SCL);
+    delay(1000);
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3C for 128x32
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+    display.clearDisplay();
+
+    logoshow();
+
+    // init the configuration
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(UWB_RST, UWB_SS, UWB_IRQ); // Reset, CS, IRQ pin
+
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachBlinkDevice(newBlink);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+}
+
+void loop()
+{
+    DW1000Ranging.loop();
+}
+
+void newRange()
+{
+    Serial.print("from: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+    Serial.print("\t Range: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+    Serial.print(" m");
+    Serial.print("\t RX power: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+    Serial.println(" dBm");
+}
+
+void newBlink(DW1000Device *device)
+{
+    Serial.print("blink; 1 device added ! -> ");
+    Serial.print(" short:");
+    Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+    Serial.print("delete inactive device: ");
+    Serial.println(device->getShortAddress(), HEX);
+}
+
+void logoshow(void)
+{
+    display.clearDisplay();
+
+    display.setTextSize(2);              // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);             // Start at top-left corner
+    display.println(F("Makerfabs"));
+    display.println(F("UWB Anchor"));
+
+    display.setTextSize(1);
+    display.setCursor(0, 40); // Start at top-left corner
+    display.println(ANCHOR_ADD);
+    display.display();
+}
